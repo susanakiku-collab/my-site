c4f5a61ea2d00fa0e9a60423e5ff45f9364265c1
susanakiku-collab <susana.kiku@gmail.com>
2026-03-07 12:14:35 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+window.APP_CONFIG = {
+
+  SUPABASE_URL: "https://cgtkbroxqdamnirjhzxy.supabase.co",
+
+  SUPABASE_ANON_KEY: "sb_publishable_veQ6nay4yBjAHz95Hv55ng_QahlWYUz",
+
+  GOOGLE_MAPS_API_KEY: "AIzaSyCHp8CAK-Z1OXWpDzill27MKLzRSNoZ5hI",
+
+  ORIGIN_LABEL: "松戸駅",
+
+  ORIGIN_LAT: 35.7843,
+
+  ORIGIN_LNG: 139.9006
+
+};
